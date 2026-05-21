0f989938f8c29e7d0934cbd5b55a0d6a292c602d
Ren0X1 <alexyanamusicpro@gmail.com>
2026-05-21 23:07:08 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Change 'pibes-mecanicos' to your GitHub repo name
-  base: '/pibes-mecanicos/',
+  base: '/',
 })
